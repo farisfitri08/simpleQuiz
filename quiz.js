4610b54de6830e03be5cb41192851af2a9712ff4
farisfitri08 <farisfitri08@gmail.com>
2022-07-27 16:06:27 -0700

    async await js for start call

--- quiz.js
+++ quiz.js
@@ -1,45 +1,46 @@
 let allQuestion = "";
 let totalMarks = 0;
 let count = 0;
-async function getUsers() {
-    let url = 'quiz.json';
-    try {
-        let res = await fetch(url);
-        return await res.json();
-    } catch (error) {
-        console.log(error);
-    }
+
+const getAllQuestionFromJson = async() => {
+    const response = await fetch("quiz.json");
+    const data = await response.json();
+
+    return data;
 }
 
-async function renderUsers(id) {
-    allQuestion = await getUsers();
+getAllQuestionFromJson().then(data => {
+    // asign data into global variable
+    allQuestion = data;
+    renderQuestion(1);
+});
+
+const renderQuestion = id => {
     totalQuestion = Object.keys(allQuestion).length;
     document.querySelector("#numberQuestion").innerText = id;
-        const ques = document.querySelector("#question");
-        ques.innerText = allQuestion[id].question;
-        
-        const options = allQuestion[id].option;
-        const optionQuestion = Object.keys(options);
-        optionQuestion.forEach((key, index) => {
+    const ques = document.querySelector("#question");
+    ques.innerText = allQuestion[id].question;
+    
+    const options = allQuestion[id].option;
+    const optionQuestion = Object.keys(options);
+    optionQuestion.forEach((key, index) => {
 
-            const breakQuestion = document.createElement("br");
-            ques.appendChild(breakQuestion);
+        const breakQuestion = document.createElement("br");
+        ques.appendChild(breakQuestion);
 
-            const para = document.createElement("input");
-            para.setAttribute('type','radio');
-            para.setAttribute('value',key);
-            para.setAttribute('name',"option");
-            para.setAttribute('id','option-'+key);
-            ques.appendChild(para);
+        const para = document.createElement("input");
+        para.setAttribute('type','radio');
+        para.setAttribute('value',key);
+        para.setAttribute('name',"option");
+        para.setAttribute('id','option-'+key);
+        ques.appendChild(para);
 
-            const labelQuestion = document.createElement("label");
-            labelQuestion.innerText = key+". "+options[key];
-            ques.appendChild(labelQuestion);
+        const labelQuestion = document.createElement("label");
+        labelQuestion.innerText = key+". "+options[key];
+        ques.appendChild(labelQuestion);
 
-        });
+    });
 }
-
-renderUsers(1);
 
 const next = () => {
     // check what question is now
@@ -58,8 +59,8 @@ const next = () => {
     if(optionC) selectAnswer = "C";
     if(optionD) selectAnswer = "D";
 
+    // if user not key in answer, dont let user move to next question
     if(selectAnswer == "") {
-        // dont move to next question
         const ques = document.querySelector("#question");
 
         const alertCheck = document.querySelector("#alertQuestion");
@@ -75,11 +76,12 @@ const next = () => {
         return;
     }
 
-    console.log(totalQuestion);
+    // total marks if correct
     if(answer == selectAnswer) {
         totalMarks++;
     }
 
+    // if last question,  we remove all div and display result
     if(questionNumber == totalQuestion) {
         const questitle = document.querySelector("#questionTitle");
         questitle.remove();
@@ -92,5 +94,5 @@ const next = () => {
         return;
     }
     // change question
-    renderUsers(questionNumber+1);
+    renderQuestion(questionNumber+1);
 }
